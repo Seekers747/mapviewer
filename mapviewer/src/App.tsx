c78c5ef17681625986c7ecef275837a4ade87933
Imraan Mohamedhoesein <imraan.mohamedhoesein@gmail.com>
2025-11-06 12:47:11 +0100

    default map done

--- mapviewer/src/App.tsx
+++ mapviewer/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import './App.css';
+
+// Fix for default marker icon in Leaflet with React
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <div>
+        <input id="start" placeholder="Start (lat,lon)" />
+        <input id="end" placeholder="End (lat,lon)" />
+        <select id="mode">
+          <option value="driving-car">Car</option>
+          <option value="cycling-regular">Bike</option>
+          <option value="foot-walking">Walk</option>
+        </select>
+        <button id="routeBtn">Calculate Route</button>
+      </div> */}
+      <div className='map'>
+        <MapContainer center={[52.3733747, 4.8833205]} zoom={13} style={{ height: '100%', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[52.3733747, 4.8833205]}>
+            <Popup>This is a free OpenStreetMap marker!</Popup>
+          </Marker>
+        </MapContainer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
